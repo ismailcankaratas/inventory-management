6551af6149c4e481d84dc8aed0617efc7fac5646
İsmail Can Karataş <ismailcankaratasss@gmail.com>
2024-12-04 13:59:50 +0300

    fix

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -6,8 +6,6 @@ const prisma = new PrismaClient();
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   try {
     const users = await prisma.users.findMany();
-    console.log("users", users);
-
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving users" });
