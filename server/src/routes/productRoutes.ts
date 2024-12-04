34bcf176fec780520645eda37b93bdd1e98c1f3b
İsmail Can Karataş <ismail.karatas@ekgyodestek.com>
2024-12-04 10:56:04 +0300

    Product added

---
+++ server/src/routes/productRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { createProduct, getProducts } from "../controllers/productController";
+
+const productRoutes = Router();
+
+productRoutes.get("/", getProducts);
+productRoutes.post("/", createProduct);
+
+export default productRoutes;
