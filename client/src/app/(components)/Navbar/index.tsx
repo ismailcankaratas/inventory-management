0c36bce5310514b6f670fc2256c414aa521b77b2
İsmail Can Karataş <ismailcankaratasss@gmail.com>
2024-12-04 11:18:30 +0300

    create product modal input update

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -33,7 +33,7 @@ const Navbar = (props: Props) => {
         <input
           type="search"
           placeholder="Start type to search groups & products"
-          className="pl-10 pr-4 py-2 w-50 md:2-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          className="pl-10 pr-4 py-2 w-full md:2-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
         />
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <Bell className="text-gray-500" size={20} />
